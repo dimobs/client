6fb3074371427e9425f3fb62b0601407a99a87a1
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-01-15 23:13:11 +0200

    master

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <footer className="footer">
-<p>Copyright designed by Dimo Karachorabadzhiev</p>
+          <p>Copyright © designed by Mihail Valkov</p>
         </footer>
-    )
+    );
 }
--- src/pages/BookList.js
+++
@@ -1,8 +0,0 @@
-import Book from "./Book";
-
-export default function BookList(props) {
-    return(
-        <ul>
- {  props.books.map(x => <Book key={x.year} {...x} />)}
-   </ul>
-)}
